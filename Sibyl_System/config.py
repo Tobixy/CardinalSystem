444fc1be9af4570325800f58c1c35b6d8554dfc5
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-03-07 20:57:53 +0530

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -1,77 +1,13 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
-import json
-import os
+from typing import Union, Optional
 
-
-def get_user_list(config, key):
-    with open("{}/SaitamaRobot/{}".format(os.getcwd(), config), "r") as json_file:
-        return json.load(json_file)[key]
-
-
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
-class Config(object):
-    LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
-
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001253661229
-    )  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (
-        -1001190806654
-    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
-    LOAD = []
-    NO_LOAD = ["rss", "cleaner", "connection", "math"]
-    WEBHOOK = False
-    INFOPIC = True
-    URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
-
-    # OPTIONAL
-    ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
-    ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
-    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "supports")
-    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "tigers")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
-    STRICT_GBAN = True
-    WORKERS = (
-        8  # Number of subthreads to use. Set as number of threads your processor uses
-    )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
-    )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
-    WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
-    )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    SPAMMERS = None
-
-
-class Production(Config):
-    LOGGER = True
-
-
-class Development(Config):
-    LOGGER = True
+API_ID: int = 2147833
+API_HASH: str = "75aaffe84071e6ceb7ab4e07b4a4a266"
+STRING_SESSION: str = "1BVtsOLUBu2GXIsLKUGTZkSgwO0AEeBcEPWmsPCPFZyZnstFNn1ltw64jD2h4MqMQxHGh_8NeJuZx5h0fW5gMg8s-4IBlz6Vw2Ks_-ttOKn7jPzdL70MbxKAlCfFwh3r_4WNIasUkineZxi7IL4qu5mmQjVWBdJYwjH-TauiLgd13MWrAy9XqT0kqyP2uG7Cw0BGAGyKKFXOzhiLUg-raH7wwcDWxxMZKhSrRtJXJT9Ga3f0A9yx2OCIhAdHZUk9t2WbgWFnJfVBpqKUullUCBy18VVuxes60an-0nFMr9jD7fZ4buWUQTKqabYeJO6xcgkSVFvtVMXLe45oR53zqGrC1d8a7x-c="
+Sibyl_logs: Union[str, int] = -1001293004442
+Sibyl_approved_logs: Union[str, int] = -1001274776877
+GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+BOT_TOKEN: str = "1615719817:AAHqQZWGvFBXVW1lTnnkv4216aYs1wNuafk"
+SIBYL = data["SKYNET"]
+INSPECTORS = data["1634756943"]
+DEVELOPERS = data["1418166549"]
+MONGO_DB_URL: str = "mongodb+srv://sybil_use:RPMvhLqJmKbCGq1x@cluster0.amty7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
