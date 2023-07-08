50e25ac738ac28b76e74ae9d030c22312a057380
Tobixy <shivamrajput69bs@gmail.com>
2023-07-08 06:33:35 +0530

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -2,6 +2,7 @@ from typing import Union, Optional
 
 API_ID: int = 29536634
 API_HASH: str = "f6d57b4713f0ecae0ac2b1fb69e136e5"
+MONGO_DB_URL: str = ""
 STRING_SESSION: str = "1BVtsOG8Bu5xD1E8NS_bfwR_yvaGAvz71VuvBczPjX1JY20lPL10YuNqY6eA2kUzNACQsMAF8CLTtsyIDtaU9DiSp6OwtlgmK6xSnkaVEOGjGL1aksnnbKb1mR_UCyixL7q9xff5Rm-awGdbfHb5OTwSxSzu2LUhpKyXaADBfdZLIf6khMdLyARe1sRCAhM3ocf6MPTZDvzTeQprH5oznJIjy_VIdY-babIMiK_ygA_fG_rBmSXpoFKrBu527ASUVeZV1geUVe8nw6SoEd_HVWLhAX0MW2epLQQmiiSW-1uOnEK9ueqfMGbyOTmSFIW2O4iL51IFHkJ1uumG-aiEB6XQag39fyI4="
 Sibyl_logs: Union[str, int] = -1001293004442 
 Sibyl_approved_logs: Union[str, int] =  -1001293004442
